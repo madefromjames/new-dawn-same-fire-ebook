37f7fdbc262d3a9f825ca1046d6a12c948f6be79
unknown <alqomorytech@gmail.com>
2026-06-17 12:57:28 +0100

    header& hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,55 @@
 import Image from "next/image";
-
+import Link from "next/link";
+import { FaDownload } from "react-icons/fa6";
+import { HiOutlineBookOpen } from "react-icons/hi2";
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <>
+    <header className="bg-[#F9F5EE] flex items-center px-8 py-4 justify-between ">
+      <nav className="flex gap-5 ml-72 ">
+        <Link href="#about" className="text-[#88631A]">About</Link>
+        <Link href="#about">What's Inside </Link>
+        <Link href="#about">Message</Link>
+        
+        </nav>  
+      <button className="flex items-center gap-3 bg-[#260406] mr-20  rounded text-white px-4 py-2 hover:bg-[#260406] "> <span><FaDownload /></span> <span>Download Ebook</span></button>
+      </header>
+      <main className="flex flex-col items-center p-8">
+      <h1 className="text-4xl "><span className="text-[#0F172A] font-black">NEW DAWN,</span> <span className="text-[#C0840B] font-black" >SAME FIRE</span></h1>
+      <p className="mt-2 text-lg">A journey of unfiltered grace</p>
+      <p className="mt-4">Thank you for being part of this special celebration. As a token of appreciation, please enjoy this complimentary copy of New Dawn, Same Fire.</p>
+
+      <div className="mt-6 flex gap-4">
+        <button className="bg-[#260406] flex gap-4 text-white px-7 py-4 rounded">
+        <span><FaDownload /></span> <span>Download Complimentary Ebook</span>
+        </button>
+        <button className="bg-[#E6E6E6] flex gap-4  text-[#260406] px-7 py-4 rounded">
+        <span><HiOutlineBookOpen /></span> <span>Request Signed Hard Copy</span>
+        </button>
+       
+      </div>
+      <div className=" relative w-full h-[200vh]">
+      <Image
+        src="/kemi.png"
+        alt="New Dawn, Same Fire"
+        className="mt-8 shadow-lg object-cover" fill priority
+      />
+</div>
+      <div className="mt-10 grid grid-cols-3 gap-6 text-center">
+        <div>
+          <h2 className="text-2xl font-bold">5K+</h2>
+          <p>Quotes Inspired</p>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+        <div>
+          <h2 className="text-2xl font-bold">10K+</h2>
+          <p>Downloads</p>
         </div>
-      </main>
-    </div>
+        <div>
+          <h2 className="text-2xl font-bold">5K+</h2>
+          <p>Shares</p>
+        </div>
+      </div>
+    </main>
+    </>
   );
 }
